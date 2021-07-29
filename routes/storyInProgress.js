eed2f27c1694d6b633989155a63ab2a36e415639
Antony <ac.chan@live.com>
2021-07-29 08:38:16 +0000

    resolve synta error in storyinprogress.ejs

--- routes/storyInProgress.js
+++ routes/storyInProgress.js
@@ -1,78 +1,34 @@
 const express = require("express");
-const { Pool } = require("pg");
 const router = express.Router();
+const { Pool } = require("pg");
 const database = require("./database");
 
 module.exports = (db) => {
-
-
-  //GET request to view storyteller page
-  router.get("/", async(req, res) => {
+  // GET request to view storyteller page
+  router.get("/", async (req, res) => {
     const stories = await database.getAllStories();
 
-    res.render("storyInProgress", {stories: stories
-    });
+    res.render("storyInProgress", { stories: stories });
   });
-
-  // router.get("/", async(req, res) => {
-  //   //const stories = await database.getAllStories();
-  //   const contributions = await database.getContributionsForStory();
-  //   //console.log(stories);
-  //   console.log(contributions);
-  //   res.render("storyInProgress", {contributions
-  //   });
-  // });
-
 
   router.post("/", (req, res) => {
-    const content = req.body.content;
     const storyId = req.body.storyId;
-    const userId = req.session.userId;
-    const contribution = {
-      userId,
-      storyId,
-      content,
-    };
 
-    database.addContribution(contribution).then((contribution) => {
-      res.redirect("storyInProgress");
-    });
+    //database query
   });
 
-    router.post("/accept", (req, res) => {
-    const contributionid = req.body.contributionsid;
-    return pool.query(
-      `UPDATE contributions set status = "accepted" where id = ${contributionid}`
-    );
-     res.redirect("/storyInProgress");
+  router.get("/:id", async (req, res) => {
+    const storyId = req.params.id;
+
+    // const email = req.session.email;
+    const contributions = await database.getContributionsForStory(storyId);
+    const story = await database.getStoryById(storyId);
+
+    // const templateVars = { shortURL: urlID, longURL: urlDatabase[req.params.shortURL].longURL, user};
+    const templateVars = { contributions, story };
+
+    return res.render("storyShow", templateVars);
   });
 
-
-  /*
-  ADD STORY
-  story teller route:
-  get back wat is typed into textbook for creating new story
-  and adding into the database for contribution under user's account
-
-  ADD CONTRIBUTION
-  contribution to story route:
-  user can contribute text to story in progress
-
-  VIEW CURRENT CONTRIBUTION
-  user can view current contribution but not accept them
-  users can up vote contribution
-
-  UP COUNT:
-  when upvoted like values goes up
-
-  ADD CONTRIBUTION TO STORY
-  owner of story can accept contribution to story
-
-  COMPLETE STORY
-  OWNER of story can submit current story as completed
-  and be viewed on completed story list
-
-
-  */
   return router;
 };
