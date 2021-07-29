104791db6f670dbbf9873245c03298fd8ca10255
Antony <ac.chan@live.com>
2021-07-29 02:28:40 +0000

    made working link for completedstory

--- server.js
+++ server.js
@@ -77,9 +77,9 @@ app.get("/", (req, res) => {
 //   res.render("login");
 // });
 
-// app.get("/error", (req, res) => {
-//   res.render("error");
-// });
+app.get("/completedStory", (req, res) => {
+  res.render("completedStory");
+});
 
 
 
