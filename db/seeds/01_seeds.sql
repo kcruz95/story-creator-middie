834c6ab12fef0fa864e1cbfd253c02255bdd2cfb
Antony <ac.chan@live.com>
2021-07-30 04:26:35 +0000

    added more test data to test

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -8,15 +8,25 @@ INSERT INTO stories (creatorId, title, isCompleted)
 VALUES
 (1,'The Ugly Barnacle', true),
 (2,'Alouette', false),
-(3,'title3', false);
+(3,'Issa Story', false),
+(4,'Rhyme on loan', true);
+(5,'This is fax no printer', false);
 
 INSERT INTO contributions (userId, storyId, content)
 VALUES
 (1, 1, 'Once there was an ugly barnacle'),
 (2, 1, 'He was SO UGLY, that everyone died.'),
-(3, 1, 'The End.');--,
--- (2, 2, 'content2'),
--- (3, 3, 'content3');
+(3, 1, 'The End.');
+(3, 4, 'They say he the host at home'),
+(1, 4, 'listening to Post Malone'),
+(2, 4, 'but he got ghost on phone'),
+(2, 4, 'now he feels most alone'),
+(3, 4, 'with his date postpone.'),
+(3, 5, 'This guy wears a cap'),
+(3, 5, 'Striaght up no cap'),
+(3, 5, 'thats fax'),
+
+
 
 INSERT INTO votes (contributionId, userId)
 VALUES
