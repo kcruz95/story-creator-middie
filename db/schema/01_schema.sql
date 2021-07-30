8cf90fa324a186b8976b5c85818a08c62302d586
jan-lab <tnlatte@outlook.com>
2021-07-30 01:06:24 -0700

    added comments and linted codes

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -1,23 +1,10 @@
-/*
-Project Sputtr
-
-User Stories
-Our middie proj is a story-making website for kids called Sputtr.
-It is set up in a way that kids can make their own stories using preset sentence templates of fairytales or kids stories or they can write themselves.
-*/
---CREATE DATABASE sputtr;
---DROP DATABASE IF EXISTS sputtr;
---CREATE DATABASE sputtr OWNER labber;
-
--- REASSIGN OWNED BY jhan TO labber;
--- ALTER DATABASE sputtr OWNER TO labber;
-
---DROP TABLE IF EXISTS widgets CASCADE;
+--drop existing tables
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS stories CASCADE;
 DROP TABLE IF EXISTS contributions CASCADE;
 DROP TABLE IF EXISTS votes CASCADE;
 
+--create tables
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -29,8 +16,7 @@ CREATE TABLE stories (
   id SERIAL PRIMARY KEY NOT NULL,
   creatorId INTEGER REFERENCES users(id) ON DELETE CASCADE,
   title VARCHAR(255) NOT NULL,
-  isCompleted BOOLEAN NOT NULL DEFAULT FALSE--,
-  --completed_at DEFAULT NOW()::timestamp; --not sure if we need this
+  isCompleted BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE contributions (
@@ -38,14 +24,6 @@ CREATE TABLE contributions (
   userId INTEGER REFERENCES users(id) ON DELETE CASCADE,
   storyId INTEGER REFERENCES stories(id) ON DELETE CASCADE,
   content TEXT NOT NULL,
-  status VARCHAR(255) NOT NULL DEFAULT 'pending', --pending/denied/accepted // ENUM('pending','accepted','denied') - data type that stores string
-  --upvote_count INTEGER NOT NULL DEFAULT 0, --to be removed
-  --sequence INTEGER DEFAULT NULL,
+  status VARCHAR(255) NOT NULL DEFAULT 'pending', --status can be pending/denied/accepted
   updatedAt TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE votes (
-  id SERIAL PRIMARY KEY NOT NULL,
-  contributionId INTEGER REFERENCES contributions(id) ON DELETE CASCADE,
-  userId INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
--- routes/login.js
+++ routes/login.js
@@ -1,44 +1,51 @@
+/*
+ * All routes for Login are defined here
+ * Since this file is loaded in server.js into /login, these routes are mounted onto /login
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
 const express = require("express");
 const router = express.Router();
 const { Pool } = require("pg");
+
+// Import helper functions
 const database = require("./database");
 
-
-// imported helper functions
-
 module.exports = (db) => {
-  // GET back the view for login page
+
+  // GET route to view login page
   router.get("/", (req, res) => {
 
     res.render("login");
   });
 
+  // POST route to check credentials
   router.post("/", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
+    // Render to error page if user is not authorized
     if (!email || !password) {
       res.render("error");
     }
-
-    // db.query(`SELECT *FROM users WHERE email = $1`, [email]).then((result) => {
-    //   const user = result.rows[0];
 
     database.getUserWithEmail(email).then((user) => {
       if (!user || user.password !== password) {
         res.render("error");
       }
+
+      // Redirect to home page with a matching user
       req.session.userId = user.id;
 
       res.redirect("/storyInProgress");
     });
   });
 
-  // POST request to logut by setting cookie to NULL
+  // POST request to logout by setting cookie to NULL
   router.post("/logout", (req, res) => {
     req.session = null;
 
-    // Redirects back to homepage
+    // Redirect back to homepage
     res.redirect("/");
   });
 
